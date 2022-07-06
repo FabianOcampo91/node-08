b4a566f7e497483e40892559b639f69dc76da290
FabianOcampo91 <faby.ocampo91@gmail.com>
2022-07-06 19:34:27 -0300

    feat: nest frameware added

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Product } from './interfaces/product.interface';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  getAllProducts(): Product[] {
+    return this.productsService.getAll();
+  }
+
+  @Get(':id')
+  find(@Param('id') id: number) {
+    return this.productsService.getId(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  createProduct(@Body() body) {
+    this.productsService.insert(body);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body) {
+    return this.productsService.update(id, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id') id: number) {
+    this.productsService.delete(id);
+  }
+}
